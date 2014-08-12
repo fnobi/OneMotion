02f2fc080c12f48fe102e84d4639405a9e38fae5
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-08-12 23:42:15 +0900

    reflectのデモ・実装

--- README.md
+++ README.md
@@ -13,17 +13,28 @@ bower install OneMotion
 See [demo page](http://fnobi.github.io/OneMotion/demo/) also.
 
 ```javascript
-var $el = $('.sample');
-var motion = new OneMotion($el, {
-    xProperty: 'top',
-    yProperty: 'left'
+// get jQuery objects for target elements
+var $sample1 = $('.sample');
+
+// init OneMotion
+var motion1 = new OneMotion($sample1, {
+    // css properties to use as x and y
+    xProperty: 'left', 
+    yProperty: 'top',
+
+    // set reflection
+    topWall: 0,
+    rightWall: 500,
+    bottomWall: 500,
+    leftWall: 0
 });
 
-$el.on('click', function () {
-    // jump to random direction
-    motion.run({
+// set events
+$sample1.on('click', function () {
+    // run to random direction
+    motion1.run({
         rad: (Math.PI * 2) * Math.random(),
-        power: 200
+        power: 500
     });
 });
 
--- src/js/OneMotion.js
+++ src/js/OneMotion.js
@@ -25,6 +25,11 @@ OneMotion.prototype.config = function (opts) {
     this.minDiff = isNaN(opts.minDiff) ? this.minDiff : opts.minDiff;
     this.clock = isNaN(opts.clock) ? this.clock : opts.clock;
     this.friction = isNaN(opts.friction) ? this.friction : opts.friction;
+
+    this.topWall = (opts.topWall !== undefined) ? opts.topWall : this.topWall;
+    this.rightWall = (opts.rightWall !== undefined) ? opts.rightWall : this.rightWall;
+    this.bottomWall = (opts.bottomWall !== undefined) ? opts.bottomWall : this.bottomWall;
+    this.leftWall = (opts.leftWall !== undefined) ? opts.leftWall : this.leftWall;
 };
 
 OneMotion.prototype.run = function (opts) {
@@ -40,20 +45,43 @@ OneMotion.prototype.run = function (opts) {
     var clock = this.clock;
     var friction = this.friction;
 
+    var topWall = this.topWall;
+    var rightWall = this.rightWall;
+    var bottomWall = this.bottomWall;
+    var leftWall = this.leftWall;
+
     var rate = 1000 / clock;
 
     var self = this;
     var $el = this.$el;
 
-    this.loop = setInterval(function () {
+    function inc() {
         self.x += Math.cos(rad) * power / rate;
         self.y += Math.sin(rad) * power / rate;
+    }
+
+    if (this.loop) {
+        clearInterval(this.loop);
+    }
+
+    this.loop = setInterval(function () {
+        inc();
 
         var css = {};
         css[xProperty] = self.x + 'px';
         css[yProperty] = self.y + 'px';
         $el.css(css);
 
+        if (self.x < leftWall && Math.cos(rad) < 0) {
+            rad = Math.PI - rad;
+        } else if (self.x > rightWall && Math.cos(rad) > 0) {
+            rad = Math.PI - rad;
+        } else if (self.y < topWall && Math.sin(rad) < 0) {
+            rad = -rad;
+        } else if (self.y > bottomWall && Math.sin(rad) > 0) {
+            rad = -rad;
+        }
+        
         power *= Math.pow(friction, 1 / rate);
 
         if (power < minDiff) {
