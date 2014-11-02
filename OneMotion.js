cadfaff7a30a4253e7868b768d509aded23f462f
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-11-03 03:02:08 +0900

    refactor.

--- OneMotion.js
+++ OneMotion.js
@@ -171,12 +171,27 @@ OneMotion.prototype.config = function (opts) {
 
 OneMotion.prototype.hit = function (opts) {
     opts = opts || {};
-
     var rad = opts.rad || (Math.PI * 2 * 0.75);
     var power = opts.power || 1;
 
-    var minDiff = this.minDiff;
+    // TODO: 既存のradとpowerとの掛け合わせ
+    this.rad = rad;
+    this.power = power;
+
+    this.stop();
+    this.time = 0;
+
+    this.trigger('hit');
+
+    var instance = this;
     var clock = this.clock;
+    this.loop = setInterval(function () {
+        instance.draw(clock);
+    }, clock);
+};
+
+OneMotion.prototype.draw = function (delta) {
+    var minDiff = this.minDiff;
     var friction = this.friction;
 
     var topWall = this.topWall;
@@ -188,64 +203,42 @@ OneMotion.prototype.hit = function (opts) {
     var stickyY = this.stickyY;
     var stickyPower = this.stickyPower;
 
-    var rate = 1000 / clock;
+    var rate = 1000 / delta;
 
-    var self = this;
+    this.time += delta;
 
-    function vectorAdd (rad1, power1, rad2, power2) {
-        var x1 = Math.cos(rad1) * power1;
-        var y1 = Math.sin(rad1) * power1;
-        var x2 = Math.cos(rad2) * power2;
-        var y2 = Math.sin(rad2) * power2;
+    this.put(
+        this.x + Math.cos(this.rad) * this.power / rate,
+        this.y + Math.sin(this.rad) * this.power / rate,
+        this.rad
+    );
 
-        return {
-            power: Math.sqrt(Math.pow(x1 + x2, 2) + Math.pow(y1 + y2, 2)),
-            rad: Math.atan2(y1 + y2, x1 + x2)
-        };
+    if (this.x < leftWall && Math.cos(this.rad) < 0) {
+        this.rad = Math.PI - this.rad;
+    } else if (this.x > rightWall && Math.cos(this.rad) > 0) {
+        this.rad = Math.PI - this.rad;
+    } else if (this.y < topWall && Math.sin(this.rad) < 0) {
+        this.rad = -this.rad;
+    } else if (this.y > bottomWall && Math.sin(this.rad) > 0) {
+        this.rad = -this.rad;
     }
 
-    this.stop();
+    if (!isNaN(stickyX) && !isNaN(stickyY) && !isNaN(stickyPower)) {
+        var xDistance = (stickyX - this.x);
+        var yDistance = (stickyY - this.y);
+        var stickyRad = Math.atan2(yDistance, xDistance);
+        var vector = OneMotion.vectorAdd(this.rad, this.power, stickyRad, (
+            stickyPower * Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2))
+        ));
+        this.rad = vector.rad;
+        this.power = vector.power;
+    }
+    
+    this.power *= Math.pow(friction, 1 / rate);
 
-    this.time = 0;
-
-    this.loop = setInterval(function () {
-        self.time += clock;
-
-        self.put(
-            self.x + Math.cos(rad) * power / rate,
-            self.y + Math.sin(rad) * power / rate,
-            rad
-        );
-
-        if (self.x < leftWall && Math.cos(rad) < 0) {
-            rad = Math.PI - rad;
-        } else if (self.x > rightWall && Math.cos(rad) > 0) {
-            rad = Math.PI - rad;
-        } else if (self.y < topWall && Math.sin(rad) < 0) {
-            rad = -rad;
-        } else if (self.y > bottomWall && Math.sin(rad) > 0) {
-            rad = -rad;
-        }
-
-        if (!isNaN(stickyX) && !isNaN(stickyY) && !isNaN(stickyPower)) {
-            var xDistance = (stickyX - self.x);
-            var yDistance = (stickyY - self.y);
-            var stickyRad = Math.atan2(yDistance, xDistance);
-            var vector = vectorAdd(rad, power, stickyRad, (
-                stickyPower * Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2))
-            ));
-            rad = vector.rad;
-            power = vector.power;
-        }
-        
-        power *= Math.pow(friction, 1 / rate);
-
-        if (power < minDiff) {
-            self.stop();
-        }
-    }, clock);
-
-    this.trigger('hit');
+    if (this.power < minDiff) {
+        this.stop();
+    }
 };
 
 OneMotion.prototype.put = function (x, y, rad) {
@@ -296,3 +289,16 @@ OneMotion.prototype.stop = function () {
     this.loop = null;
     this.trigger('stop');
 };
+
+OneMotion.vectorAdd = function (rad1, power1, rad2, power2) {
+    var x1 = Math.cos(rad1) * power1;
+    var y1 = Math.sin(rad1) * power1;
+    var x2 = Math.cos(rad2) * power2;
+    var y2 = Math.sin(rad2) * power2;
+
+    return {
+        power: Math.sqrt(Math.pow(x1 + x2, 2) + Math.pow(y1 + y2, 2)),
+        rad: Math.atan2(y1 + y2, x1 + x2)
+    };
+};
+
